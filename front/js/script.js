c2f9c34e4055ee57f4aa43ff6878af7261497c3f
Noeh <noehledra@gmail.com>
2022-10-11 16:39:07 +0200

    Fist commit

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Ne pas oublier
+ * de bien commenter ! Notamment les fonctions.
+ *  /!\ /!\
+ * (ceci est ce que l'on appelle un commentaire
+ * au format JSDoc)
+ */
+//__________________________________________________
+
+
